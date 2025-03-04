7820b858f06520ff52a44436268c498f3ec36cbb
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-04 19:17:27 +0100

    string syntax

---
+++ 002-Strings.py
@@ -0,0 +1,29 @@
+smth = "hello world i am learning python" 
+
+newSmth = smth.split()
+
+print(smth.upper())
+print(smth.replace("e","X"))
+
+print(smth[0])
+
+#String[begin:last:counting how many times]
+
+print(smth[1:6])
+#ello
+print(smth[1:6:3])
+#eo
+print(smth[1:])
+#ello world i am learning python
+print(smth[:11])
+#hello world
+print(smth[3::3])
+#lwlimengyo counted all space
+print(smth[::3])
+#hlwlimengyo
+
+print(len(smth))
+#32
+
+print(smth[-1])
+#n
