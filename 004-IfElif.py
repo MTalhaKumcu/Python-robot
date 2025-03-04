22564df5c822094b524befe4f8948fdd2e0b06e6
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-04 20:10:31 +0100

    if elif  else

---
+++ 004-IfElif.py
@@ -0,0 +1,29 @@
+num01 = 10
+num02 = 11
+"""
+more than one command line
+
+"""
+
+if num01 > num02:
+    print("buyuktur")
+
+print("if`in disindadir")
+
+if num02 > num01:
+    print(f"{num02}`dan - {num01} buyuktur")
+elif num02 == num01:
+    print(f"{num02} - {num01} buyuktur")     
+else:
+    print(f"{num02}- {num01} buyuktur")     
+
+print("-----------------------------------")
+
+sentence = "this is a sentence !"
+
+if "!" in sentence:
+    print("yes , there is")
+else:
+    print("no , there is not")
+
+print("-----------------------------------")
