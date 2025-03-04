531d537dcf2fd49c8a7666bfe2d6ed2f440b800f
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-04 19:42:39 +0100

    converting type

---
+++ 003-TypeConvert.py
@@ -0,0 +1,30 @@
+numer01 = "10"
+numer02 = 20
+
+
+print(type(numer01))
+print(type(numer02))
+
+#bu liste dictionary set`lerde kullanilabilir 
+print("-------------------------------")
+print(type(int(numer01)))
+print(type(float(numer01)))
+print(type(str(numer02)))
+print("-------------------------------")
+print(int(numer01))
+print(float(numer01))
+print(str(numer02))
+print(int(numer01)+10)
+print("-------------------------------")
+
+name = "helloooo"
+age=28
+
+print(name+" world " +"my age "+str(age))
+print("-------------------------------")
+
+print(f"again {name} my age {age}")
+print("-------------------------------")
+
+print(f"\t again {name} \n my age {age}")
+#\ kacis ifadesidir
