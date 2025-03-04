6ab14ece1b65aa458d18bd754832ea358a5197a0
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-04 20:50:59 +0100

    dictionary

--- 007-Dictionary.py
+++ 007-Dictionary.py
@@ -1,1 +1,36 @@
 print("-----Dictionary------")
+"""
+Java`daki json format gibidir.
+"key" : "value"
+icerisine bir yeni veriler eklenebilir
+
+"""
+
+myDictionary = {
+
+    "name" : "Talha",
+    "age" : 28,
+    "country" : "Poland",
+    "marital status": True
+}
+
+myDictionary["yeniKey"] = "new Value"
+print(myDictionary)
+
+print("-------------------------")
+
+ludzi={
+    "id1" : {
+        "name" : "michael",
+        "age" : 15,
+        "phone" : ["1123123","1231231"]
+    },
+    "id2" : {
+          "name" : "nedim",
+        "age" : 13,
+        "phone" : ["1123123","1231231"]
+    }
+}
+print(ludzi)
+print("-------------------------")
+
