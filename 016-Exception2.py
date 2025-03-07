acd80cefb7ce19b96e03a68aef00e4dd8817f7ac
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-07 23:24:11 +0100

    Exception

---
+++ 016-Exception2.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        num1 = int(input("number 1: "))
+        num2 = int(input("number 2: "))
+
+        print(num1 + num2)
+        break # buradaki break ile asagidaki else ayni islemi gorur
+    except:
+        print("lutfen sayi girin! ")
+    """
+    else:
+        break
+    """
