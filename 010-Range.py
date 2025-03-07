7e1aa1e1af9be8398f4a036b8461e90ebceba41a
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-07 22:04:15 +0100

    range

---
+++ 010-Range.py
@@ -0,0 +1,12 @@
+#range`de sonra sayi dahil degildir`
+
+numberList = list(range(10,15))
+print(numberList)
+
+list2 = list(range(40))
+print(list2)
+#son sayi verilmeli cunku en son olan odur
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+#21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+
+
