f8ea331ec4396de6d64e6c56b4eed1dda9a574f7
Mtalhakumcu <mkumcu151@gmail.com>
2025-03-13 10:51:20 +0100

    custom Lib using

--- RobotFramework/009-customLib.robot
+++ RobotFramework/009-customLib.robot
@@ -7,6 +7,11 @@ Library    CustomLib.py
 Test01
     ${toplama}=    Toplama Islemi    15    20
     Log To Console    ${toplama}
-    
+
+
+  #  Sayilari Topla    10    12
+    yenikeyword    50    60
+    #txt anlatiliyor
+
 *** Keywords ***
 
--- RobotFramework/CustomLib.py
+++ RobotFramework/CustomLib.py
@@ -1,2 +1,11 @@
+from robot.api import logger
+from robot.api.deco import keyword
+
+
 def toplamaIslemi(sayi1, sayi2):
     return int(sayi1) + int(sayi2)
+
+
+@keyword(name="yenikeyword")
+def sayilari_topla(sayi1, sayi2):
+    logger.console(int(sayi1) + int(sayi2))
